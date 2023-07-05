46fe4e77757db93a394da627845667e4f46e6dbe
Nuradiyana <me@nooradiana.com>
2023-07-05 16:43:06 +0700

    Register response interface

--- src/LaravelBudgetServiceProvider.php
+++ src/LaravelBudgetServiceProvider.php
@@ -4,11 +4,20 @@ declare(strict_types=1);
 
 namespace Inisiatif\LaravelBudget;
 
+use Inisiatif\LaravelBudget\Contracts;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Inisiatif\LaravelBudget\Http\Responses\FilterBudgetResponse;
+use Inisiatif\LaravelBudget\Http\Responses\FetchOneBudgetResponse;
 
 final class LaravelBudgetServiceProvider extends PackageServiceProvider
 {
+    public function registeringPackage(): void
+    {
+        $this->app->bind(Contracts\FilterBudgetResponse::class, FilterBudgetResponse::class);
+        $this->app->bind(Contracts\FetchOneBudgetResponse::class, FetchOneBudgetResponse::class);
+    }
+
     public function configurePackage(Package $package): void
     {
         $package
