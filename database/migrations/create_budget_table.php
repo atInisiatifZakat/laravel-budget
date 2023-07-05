5c55dd8ea523def3e8a7ed32c70aadb61cb4e94f
nuradiyana <nuradiyana@users.noreply.github.com>
2023-07-05 05:08:24 +0000

    Fix styling

--- database/migrations/create_budget_table.php
+++ database/migrations/create_budget_table.php
@@ -1,14 +1,16 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+declare(strict_types=1);
+
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
-    public function up()
+    public function up(): void
     {
-        Schema::create('laravel_budget_table', function (Blueprint $table) {
+        Schema::create('laravel_budget_table', function (Blueprint $table): void {
             $table->id();
 
             // add fields
--- src/LaravelBudgetServiceProvider.php
+++ src/LaravelBudgetServiceProvider.php
@@ -1,11 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Inisiatif\LaravelBudget;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
-class LaravelBudgetServiceProvider extends PackageServiceProvider
+final class LaravelBudgetServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
