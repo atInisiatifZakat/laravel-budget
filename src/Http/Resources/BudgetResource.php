60fcd382213de7c7d0b0c09501b648681adc5421
ihsanfikri12 <ihsanfikri12@users.noreply.github.com>
2024-08-30 06:39:35 +0000

    Fix styling

--- src/Http/Resources/BudgetResource.php
+++ src/Http/Resources/BudgetResource.php
@@ -6,14 +6,13 @@ namespace Inisiatif\LaravelBudget\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 use Inisiatif\LaravelBudget\Contracts\HasBudget;
-use Inisiatif\LaravelBudget\LaravelBudget;
 
 final class BudgetResource extends JsonResource
 {
     public function toArray($request): array
     {
         if (! $this->resource instanceof HasBudget) {
-            throw new \RuntimeException('$resource must be instanceof ' . HasBudget::class);
+            throw new \RuntimeException('$resource must be instanceof '.HasBudget::class);
         }
 
         return [
@@ -26,7 +25,7 @@ final class BudgetResource extends JsonResource
             'balance_amount' => $this->resource->getBalance(),
             'is_over' => $this->resource->isOver(),
             'is_limit_reached' => $this->resource->isLimitReached(),
-            'version' => $this->resource->getVersion()
+            'version' => $this->resource->getVersion(),
         ];
     }
 }
